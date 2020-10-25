d23acc05d8b4f76806e60a0686a5553b2793f59a
Teresa Pham <teresakypham@gmail.com>
2020-10-25 11:57:55 +1100

    Max Product of Three problem

---
+++ lessons/max_product_of_three.py
@@ -0,0 +1,10 @@
+def solution(A):
+    A.sort()
+    N = len(A)
+    
+    return max(A[N-1] * A[N-2] * A[N-3], A[0] * A[1] * A[N-1])
+
+if __name__ == "__main__":
+    print(solution([-3,1,2,-2,5,6]))
+    print(solution([-5,-5,5,4]))
+    print(solution([-2,4,1,6]))
