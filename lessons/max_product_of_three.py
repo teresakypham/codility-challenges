59a8d37fef4f46bb3e15c2c65ed6422275bde9ef
Teresa Pham <teresakypham@gmail.com>
2020-10-25 11:58:52 +1100

    Added comment to Max Product of Three problem

--- lessons/max_product_of_three.py
+++ lessons/max_product_of_three.py
@@ -2,6 +2,7 @@ def solution(A):
     A.sort()
     N = len(A)
     
+    # Need to consider case where two negative multiples = positive
     return max(A[N-1] * A[N-2] * A[N-3], A[0] * A[1] * A[N-1])
 
 if __name__ == "__main__":
